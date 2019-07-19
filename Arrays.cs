06dab537850f4ae2d8d7c1d2520ddb68ecfc420e
cre <satish_j80@yahoo.com>
2019-07-19 16:56:36 -0700

    Adding Algorithms to NetCore

---
+++ Arrays.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+public class Arrays
+{
+    //https://leetcode.com/problems/partition-labels/
+    public void PartitionStringWithUniqueChars()
+    {
+        Console.WriteLine(PartitionStringWithUniqueChars("abacbdefedghiho"));
+    }
+
+    private IList<int> PartitionStringWithUniqueChars(string input)
+    {
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        IList<int> res = new List<int>();
+        List<int> split = new List<int>();
+
+        int idx = 0;
+
+        for(; idx < input.Length; idx++)
+        {
+            if (!map.ContainsKey(input[idx]))
+            {
+                map.Add(input[idx],idx);
+            }
+            else
+            {
+                split.Add(idx);
+                map[input[idx]] = idx;
+            }
+        }
+
+        idx = 0;
+        int start = 0;
+        int cur = start;
+        int end = split[idx++];
+
+        while (cur < input.Length)
+        {
+            while (cur < end)
+            {
+                if (map[input[cur]] > end)
+                {
+                    end = map[input[cur]];
+                    while (idx + 1 < split.Count && split[idx] < end)
+                    {
+                        idx ++;
+                    }
+                }
+
+                cur++;
+            }
+            
+            res.Add(end-start + 1);
+
+            start = end+1;
+            cur = start;
+            end = split[idx];
+            
+            if (idx + 1 < split.Count)
+             {
+                 idx++;
+             }
+        }
+
+        return res;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays arrObj = new Arrays();
+            arrObj.PartitionStringWithUniqueChars();
+        }
+    }
+}
