f6ae545129f0948cb3da739853a2fc941751d301
cre <satish_j80@yahoo.com>
2019-07-21 19:45:49 -0700

    Added better way to count palindromic substring

--- DP.cs
+++ DP.cs
@@ -2,10 +2,11 @@ using System;
 
 public class DP
 {
+    //https://leetcode.com/problems/palindromic-substrings/
     // Return the palindromic sub strings for a given string
     public void FindPalindromeSubstrings()
     {
-        Console.WriteLine(FindPalindromeSubstrings("abbacfgfr"));
+        Console.WriteLine(countSubstrings("abbaefgfl"));
     }
 
     private int FindPalindromeSubstrings(string input)
@@ -35,16 +36,31 @@ public class DP
             {
                 if (dp[row, col] == 1)
                 {
-                    var r = row + 1;
-                    var c = col +1;
+                    var r = row;
+                    var c = col;
 
                     bool found = false;
 
                     while(r < input.Length && c < input.Length && dp[r,c] == 1)
                     {
-                        found = true;
-                        dp[r++, c++] = 0;
+                        if (r+1 < input.Length && c+1 < input.Length )
+                        {
+                            if (input[c+1] == input[c] && rev[r+1] == rev[r])
+                            {
+                                count +=1;
+                            }
+                            else
+                            {
+                                found = true;
+                            }
+                        }
+
+                        dp[r, c] = 0;
+
+                        r++;
+                        c++;
                     }
+
                     if (found)
                     {
                         found = false;
@@ -56,4 +72,25 @@ public class DP
 
         return count;
     }
+
+    public int countSubstrings(String s) 
+    {
+        int n = s.Length;
+        int res = 0;
+        bool[,] dp = new bool[n,n];
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            for (int j = i; j < n; j++) 
+            {
+                dp[i,j] = s[i] == s[j] && (j - i < 3 || dp[i + 1, j - 1]);
+                if(dp[i,j])
+                {
+                    ++res;
+                }
+            }
+        }
+        
+        return res;
+    }
+
 }
